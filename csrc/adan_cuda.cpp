801b60ca2f37fefa6d24255bcec8ddfd48b1380d
xingyuxie <420603630@qq.com>
2023-05-16 08:13:32 +0000

    refine adan.py

--- csrc/adan_cuda.cpp
+++ csrc/adan_cuda.cpp
@@ -22,10 +22,10 @@ void adan_launcher(
     float bias_correction3_sqrt
 );
 
-// Macro definitions for checking tensor properties
-#define CHECK_CUDA(x) AT_ASSERTM(x.is_cuda(), #x " must be a CUDA tensor")
-#define CHECK_CONTIGUOUS(x) AT_ASSERTM(x.is_contiguous(), #x " must be contiguous")
-#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+// // Macro definitions for checking tensor properties
+// #define CHECK_CUDA(x) AT_ASSERTM(x.is_cuda(), #x " must be a CUDA tensor")
+// #define CHECK_CONTIGUOUS(x) AT_ASSERTM(x.is_contiguous(), #x " must be contiguous")
+// #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
 // Main functions
 void F_adan(
